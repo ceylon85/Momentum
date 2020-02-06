b421ae2d89fe3772553f12a2070f945b267dcdb2
ceylon85 <canleez123@gmail.com>
2020-02-06 17:55:20 +0900

    background

--- src/background.js
+++ src/background.js
@@ -1,15 +1,16 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBER = 10;
+const IMG_NUMBER = 10; //img dir에 파일이 10개
 
 function paintImage(imgNumber){
-    const image = new Image();
-    image.src = `./img/${imgNumber + 1}.jpg`
+    const image = new Image(); //새로운 image를 받고
+    image.src = `./img/${imgNumber + 1}.jpg` 
     image.classList.add("bgImage");
     body.appendChild(image);
 }
 
-function genRandom(){
+function genRandom(){ //math 함수를 사용,floor를 사용해 나머지 값을 버리고 
+    //random 함수를 사용하여 1~10까지 정수를 리턴
     const number = Math.floor(Math.random()* IMG_NUMBER);
     return number;
 }
